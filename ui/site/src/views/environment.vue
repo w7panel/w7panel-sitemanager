a3393fc9b9013987c5f4abbdc8e5710834fee9ae
santonese <133618906+santonese@users.noreply.github.com>
2026-06-05 11:32:27 +0800

    提示样式调整

--- ui/site/src/views/environment.vue
+++ ui/site/src/views/environment.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-f2" style="min-height:100%;">
+  <div style="min-height:100%;">
     <div>
       <div>
         <div class="bg-white bg-padding pb-24" style="border-top:1px solid #EEEEEE;">
