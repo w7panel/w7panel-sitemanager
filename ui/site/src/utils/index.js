207440e07dee9436063361771ec90d5971898c5c
rxw <1120309488@qq.com>
2026-07-13 16:40:26 +0800

    优化接口授权

--- ui/site/src/utils/index.js
+++ ui/site/src/utils/index.js
@@ -9,7 +9,7 @@ const myAxios = axios.create({
 
 myAxios.interceptors.request.use(config => {
     config.headers[SITE_MANAGER_TOKEN_HEADER] = getSiteManagerToken()
-    config.headers['AuthorizationX'] = getPanelToken()
+    config.headers['Authorization'] = getPanelToken()
     return config
 }, err => {
     Promise.reject(err)
